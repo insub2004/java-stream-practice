3d6da6417fe2562f1ea28ccb4f7ff5ac5951848b
insub2004 <insub2004@naver.com>
2026-02-11 23:54:47 +0900

    Problem3 solved 1. mapToDouble(..).sum() 2. reduce(시작값, (누적값, 새로운값) -> 누적값 + 새로운값)

--- src/main/java/problem/easy/Problem3.java
+++ src/main/java/problem/easy/Problem3.java
@@ -11,7 +11,17 @@ public class Problem3 {
      * @return 요소들의 합계
      */
     public static double sumOfList(List<Double> numbers) {
-        // 여기에 코드 작성
-        return 0.0;
+        // 방법 1번
+        // 중간 연산 : DoubleStream 숫자 전용으로 변환 : mapToDouble(Double::doubleValue)를 사용
+        // 최종 연산 : sum으로 모든 값 더하기
+//        return numbers.stream()
+//                .mapToDouble(Double::doubleValue)   // 메서드 참조 방식(n -> n.doubleValue()와 똑같음)
+//                .sum();
+
+        // 방법 2번
+        //reduce(처음 시작하는 합계값, (지금까지 쌓인 합계, 리스트에서 새오 꺼낸 숫자) -> 지금까지 쌓인 합계 + 리스트에서 새오 꺼낸 숫자)
+        return numbers.stream()
+                .reduce(0.0, (a,b) -> a + b); // Double::sum 처럼 메서드 참조를 사용해도 좋음
+
     }
 }
