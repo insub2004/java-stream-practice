2afff6884e6b5467fc1ef1aded7d89dc68c86e94
insub2004 <insub2004@naver.com>
2026-02-11 23:35:10 +0900

    Problem1 solved

--- src/main/java/problem/easy/Problem1.java
+++ src/main/java/problem/easy/Problem1.java
@@ -11,6 +11,11 @@ public class Problem1 {
      * @return 길이가 5 이상인 문자열만 포함하는 리스트
      */
     public static List<String> filterStrings(List<String> strings) {
-        return null;
+        // 1. 생성
+        // 2. 중간 연산 -> 길이가 5이상인 문자열 필터링
+        // 3. 최종 연산 -> 스트림 데이터를 다시 리스트로 묶기
+        return strings.stream()
+                .filter(s -> s.length() >= 5)  // filter(Predicate) -> Predicate의 경우 boolean을 반환하는 추상 메서드가 있다.
+                .toList();
     }
 }
