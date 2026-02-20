d39d67f3cf5de2fdd7137b5eb2bb30fbbfa6a1c2
insub2004 <insub2004@naver.com>
2026-02-20 23:53:50 +0900

    Problem9 solved 1. 조건에 맞지않는 요소도 거르지 않고 그대로 놔두려면, filter 말고 map을 사용하자

--- src/main/java/problem/easy/Problem9.java
+++ src/main/java/problem/easy/Problem9.java
@@ -1,6 +1,7 @@
 package problem.easy;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem9 {
 
@@ -11,7 +12,13 @@ public class Problem9 {
      * @return 변환된 문자열 리스트
      */
     public static List<String> transformStrings(List<String> strings) {
-        // 여기에 코드 작성
-        return null;
+        // 틀렸던 점
+        // 1. filter를 사용해 길이가 3이상인 문자열만 고르려고 했다 -> 3미만인 문자열들은 사라짐(문제의도는 그대로 두어야함!)
+        // 정답
+        // map(..) : 리스트의 각 요소를 하나씩 꺼내서 우리가 원하는 형태로 가공해야함 + 삼항연산다
+        return strings.stream()
+                .map(string -> string.length() >= 3 ? string.toUpperCase() : string)
+                .collect(Collectors.toList());
+
     }
 }
