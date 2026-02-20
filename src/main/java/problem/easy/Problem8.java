c6d4a02541c41414479e4e7a632dedb623d579b2
insub2004 <insub2004@naver.com>
2026-02-20 23:47:34 +0900

    Problem8 solved 1. 짝수 찾기 : filter(v->v%2==0) 2. 그 중 첫번째 요소 : findFirst() // 반환타입이 Optional임

--- src/main/java/problem/easy/Problem8.java
+++ src/main/java/problem/easy/Problem8.java
@@ -12,7 +12,11 @@ public class Problem8 {
      * @return 첫 번째 짝수 (존재하지 않으면 Optional.empty())
      */
     public static Optional<Integer> findFirstEven(List<Integer> numbers) {
-        // 여기에 코드 작성
-        return Optional.empty();
+
+        // "첫"번째 짝수를 찾을 것
+
+        return numbers.stream()
+                .filter(value -> value % 2 == 0)  // 2로 나눈 나머지가 0인 요소만 거르기
+                .findFirst();                            // 그것들 중에 첫번째만 Optional 반환
     }
 }
